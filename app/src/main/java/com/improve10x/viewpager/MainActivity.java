b2321f89cdb745b169c8ad68defbd894e7821120
LENOVO <lakshmi.sannithi@improve10x.com>
2023-04-06 12:18:53 +0530

    I created Viewpager whatsapp

---
+++ app/src/main/java/com/improve10x/viewpager/MainActivity.java
@@ -0,0 +1,31 @@
+package com.improve10x.viewpager;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    ViewPager2 whatsappVp;
+    ViewpagerAdapter adapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+        setupAdapter();
+        connectAdapter();
+    }
+
+    private void connectAdapter() {
+        whatsappVp.setAdapter(adapter);
+    }
+
+    private void setupAdapter() {
+       adapter = new ViewpagerAdapter(this);
+    }
+
+    private void initViews() {
+        whatsappVp = findViewById(R.id.whatsapp_vp);
+    }
+}
