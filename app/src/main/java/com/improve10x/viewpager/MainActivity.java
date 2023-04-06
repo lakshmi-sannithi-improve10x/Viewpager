7cc9078c7ff7dfaebeb9d9eb9c8631f05e443042
LENOVO <lakshmi.sannithi@improve10x.com>
2023-04-06 15:47:35 +0530

    I added Tablayout in whatsapp

--- app/src/main/java/com/improve10x/viewpager/MainActivity.java
+++ app/src/main/java/com/improve10x/viewpager/MainActivity.java
@@ -1,13 +1,19 @@
 package com.improve10x.viewpager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.widget.Switch;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
    private ViewPager2 whatsappVp;
   private   ViewpagerAdapter adapter;
+  private TabLayout tabTl;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,6 +21,25 @@ public class MainActivity extends AppCompatActivity {
         initViews();
         setupAdapter();
         connectAdapter();
+        setupTabWithViewpager();
+    }
+
+    private void setupTabWithViewpager() {
+        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tabTl, whatsappVp, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch (position){
+                    case 1:tab.setText("Status");
+                    break;
+                    case 2:tab.setText("Calls");
+                    break;
+                    case 0:
+                    default:tab.setText("Chats");
+
+                }
+            }
+        });
+        tabLayoutMediator.attach();
     }
 
     private void connectAdapter() {
@@ -27,5 +52,6 @@ public class MainActivity extends AppCompatActivity {
 
     private void initViews() {
         whatsappVp = findViewById(R.id.whatsapp_vp);
+        tabTl = findViewById(R.id.tab_tl);
     }
 }
--- app/src/main/java/com/improve10x/viewpager/ViewpagerAdapter.java
+++ app/src/main/java/com/improve10x/viewpager/ViewpagerAdapter.java
@@ -14,12 +14,11 @@ public class ViewpagerAdapter extends FragmentStateAdapter {
     @Override
     public Fragment createFragment(int position) {
         switch (position) {
-            case 0:
-                return new ChatsFragment();
             case 1:
                 return new StatusFragment();
             case 2:
                 return new CallsFragment();
+            case 0:
             default:
                 return new ChatsFragment();
         }
